b3fdb0787b12cffc57d085b3b124654a0584a9a1
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-10 15:40:32 -0800

    can get with user id

--- api/server.js
+++ api/server.js
@@ -9,6 +9,9 @@ server.use(express.json())
 
 //ENDPOINTS
 
+//POST
+
+
 //GET
 server.get('/api/users', async (req, res) => {
     try {
@@ -16,6 +19,20 @@ server.get('/api/users', async (req, res) => {
         res.json(users)
     } catch (err) {
         res.status(500).json({ message: err.message })
+    }
+})
+
+server.get('/api/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const user = await User.findById(id)
+        if (!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            res.status(200).json(user)
+        }
+    } catch (err) {
+        res.status(500).json({ message: "The user information could not be retrieved" })
     }
 })
 
