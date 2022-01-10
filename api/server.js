c30735c997a2ad7e29e443f8427b66798db734ad
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-10 15:27:07 -0800

    can get all users

--- api/server.js
+++ api/server.js
@@ -1,7 +1,22 @@
 // BUILD YOUR SERVER HERE
 
 const express = require('express')
+const User = require('./users/model.js')
 
 const server = express()
+
+server.use(express.json())
+
+//ENDPOINTS
+
+//GET
+server.get('/api/users', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.json(users)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
