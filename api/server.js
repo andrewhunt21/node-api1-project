8e73baab7b97d647cf1918c9b96a61265c349a37
andrewhunt21 <andrewhunt21@icloud.com>
2022-01-10 16:07:04 -0800

    can post

--- api/server.js
+++ api/server.js
@@ -10,7 +10,21 @@ server.use(express.json())
 //ENDPOINTS
 
 //POST
-
+server.post('/api/users', async (req, res) => {
+    try {
+        const { name, bio } = req.body
+        console.log(name, bio)
+        const newUser = await User.insert({ name, bio })
+        if (!newUser.name || !newUser.bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        } else {
+            console.log(newUser)
+            res.status(201).json(newUser)
+        }
+    } catch (err) {
+        res.status(500).json({ message: "There was an error while saving the user to the database" })
+    }
+})
 
 //GET
 server.get('/api/users', async (req, res) => {
